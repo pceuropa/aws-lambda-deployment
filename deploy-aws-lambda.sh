e8ee2c815fc69986654a4d8974005e112f70ebc7
Rafal Marguzewicz <info@pceuropa.net>
2018-10-03 13:58:44 +0200

    fix name function until update

--- deploy-aws-lambda.sh
+++ deploy-aws-lambda.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export TESTMODE=1
 
 R='\e[31m'
 G='\e[32m'
@@ -6,8 +7,11 @@ Y='\e[33m'
 C='\e[96m'
 NC='\033[0m' # No Color
 PYTHON_VER='3.7'
+region='ap-southeast-2'
 LAMBDA_RUNTIME='python3.6'
-LAMBDA_ROLE='arn:aws:iam::0008907699:role/role_name'
+LAMBDA_ROLE='arn:aws:iam::890769921003:role/functionStopAllec2WithoutOwner'
+
+echo region: $region
 
 function create_function {
 
@@ -17,7 +21,7 @@ function create_function {
   if [[ "$response" =~ ^(yes|y)$ ]]
   then
       cd $VIRTUAL_ENV/lib/python$PYTHON_VER/site-packages/
-      zip -r --quiet ../../../../project.zip *
+      zip -r --quiet ../../../../project.zip *.py
       cd ../../../../
   fi
 
@@ -26,7 +30,7 @@ function create_function {
   response=${response}    # tolower
   if [[ "$response" =~ ^(yes|y)$ ]]
   then
-      zip -g project.zip *
+      zip -g project.zip *.py
   fi
 
   printf "${Y}deploy.zip? [y/N]${NC} "
@@ -37,16 +41,18 @@ function create_function {
     read -r -p "FunctionName:  " function_name
     read -r -p "Handler:  " handler
 
-    aws lambda create-function --function-name ${function_name} --runtime $LAMBDA_RUNTIME --handler ${handler}  --role ${LAMBDA_ROLE} \
+    aws lambda create-function --region ${region} --function-name ${function_name} --runtime $LAMBDA_RUNTIME --handler ${handler}  --role ${LAMBDA_ROLE} \
     --zip-file fileb://project.zip --description "Description"  >> lambda_config.json 2>&1
+    echo ${function_name} >> lambda_config.json
     cat lambda_config.json
   fi
 }
 
 function read_function {
     read -r -p "Function name:  " function_name
-    aws lambda get-function --function-name ${function_name}
+    aws lambda get-function --region ${region} --function-name ${function_name}
 }
+
 function update_function {
 
   printf "${Y}Zip requirements packages? [y/N]${NC} "
@@ -66,8 +72,11 @@ function update_function {
   then
     zip -g project.zip *.py
     sed -n 2p lambda_config.json
-    read -r -p "Function name:  " function_name
-    aws lambda update-function-code --function-name ${function_name}  --zip-file fileb://project.zip
+    function_name="$(tail -n 1 lambda_config.json)"
+    # optional from keybords
+    # read -r -p "Function name:  " function_name
+
+    aws lambda update-function-code --region ${region} --function-name ${FUNCTION_NAME}  --zip-file fileb://project.zip
   fi
 }
 
@@ -77,11 +86,12 @@ function delete_function {
     cat lambda_config.json
   printf "${R}Function name: ${NC} "
     read -r function_name
-    aws lambda delete-function --function-name ${function_name}
+    aws lambda delete-function --function-name ${function_name} --region ${region}
 }
 
 
-printf "Action: ${Y}[C]reate/[U]pdate/[R]ead/[D]elete? [c/r/u/d]${NC} "
+
+printf "Action: ${Y}[C]reate/[U]pdate/[R]ead/[D]elete lambda function ?[c/r/u/d]${NC} "
   read response
   response=${response}    # tolower
 
